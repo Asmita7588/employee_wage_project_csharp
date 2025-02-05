d5d51bbcc8b86985c75760c030a6ffa00789f565
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-05 10:43:14 +0530

    Check Employee Present Or Not

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random  random = new Random();
+
+        int attendence = random.Next(0, 10);
+        if (attendence == 0)
+        {
+            Console.WriteLine("Employee is Absent");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Present");
+        }
+    }
+}
