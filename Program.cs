637d411536c403508e51b749a800c1ac8d6a7acc
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-05 11:55:06 +0530

    Added Part time hours and calculate

--- Program.cs
+++ Program.cs
@@ -1,26 +1,42 @@
-﻿internal class Program
+﻿using System.ComponentModel.Design;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-       const int wage_Per_Hours = 20;
-       const int full_Day_Hours = 8;
+        const int wage_Per_Hours = 20;
+        const int full_Day_Hours = 8;
+        const int part_Time_Hours = 4;
 
-        Random  random = new Random();
+        int full_Time = 1;
+        int part_Time = 2;
+
+        Random random = new Random();
 
         int daily_Employee_Wage;
+        int work_Hours;
 
-        int attendence = random.Next(0, 10);
-        if (attendence == 0)
+        int attendence = random.Next(0, 3);
+        if (attendence == full_Time)
         {
-            Console.WriteLine("Employee is Absent");
-            daily_Employee_Wage = 0;
+            Console.WriteLine("Employee is Full Time");
+            work_Hours = full_Day_Hours;
+            daily_Employee_Wage = wage_Per_Hours * work_Hours;
             Console.WriteLine("Daily Employee Wage = " + daily_Employee_Wage);
+        }
+        else if (attendence == part_Time)
+        {
+
+            Console.WriteLine("Employee is Part Time");
+            work_Hours = part_Time_Hours;
+            daily_Employee_Wage = wage_Per_Hours * work_Hours;
+            Console.WriteLine("Daily Employee Wage = " + daily_Employee_Wage);
+
         }
         else
         {
-            Console.WriteLine("Employee is Present");
-            daily_Employee_Wage = wage_Per_Hours * full_Day_Hours;
-            Console.WriteLine("Daily Employee Wage = " + daily_Employee_Wage);
+            Console.WriteLine("Employee is Absent");
         }
     }
-}
+}   
+
