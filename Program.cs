21f452eca8dcfcf62c38b3575d6ae2767020993e
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-05 11:00:34 +0530

    Calculate Daily Employee Wage

--- Program.cs
+++ Program.cs
@@ -2,16 +2,25 @@
 {
     private static void Main(string[] args)
     {
+       const int wage_Per_Hours = 20;
+       const int full_Day_Hours = 8;
+
         Random  random = new Random();
+
+        int daily_Employee_Wage;
 
         int attendence = random.Next(0, 10);
         if (attendence == 0)
         {
             Console.WriteLine("Employee is Absent");
+            daily_Employee_Wage = 0;
+            Console.WriteLine("Daily Employee Wage = " + daily_Employee_Wage);
         }
         else
         {
             Console.WriteLine("Employee is Present");
+            daily_Employee_Wage = wage_Per_Hours * full_Day_Hours;
+            Console.WriteLine("Daily Employee Wage = " + daily_Employee_Wage);
         }
     }
 }
--- obj/Debug/net8.0/employeeWageProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/employeeWageProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("employeeWageProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d51bbcc8b86985c75760c030a6ffa00789f565")]
 [assembly: System.Reflection.AssemblyProductAttribute("employeeWageProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("employeeWageProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
